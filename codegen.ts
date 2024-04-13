0e0bce70bb291138fafca4dae8bae473cd8256c6
yoheisuda <y1405705@gmail.com>
2024-04-13 21:32:48 +0900

    graphql-codegenの仕様が変わったため、合わせて変更

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,15 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
+// SCHEMA_URLは.env.localに追加
+// https://{projectのreference_id}/graphql/v1　の形式で作成する
 const config: CodegenConfig = {
-  schema: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/graphql/v1?apikey=${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+  schema: {
+    [process.env.NEXT_PUBLIC_SCHEMA_URL!]: {
+      headers: {
+        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      },
+    },
+  },
 
   documents: ["**/*.tsx", "**/*.ts"],
 
